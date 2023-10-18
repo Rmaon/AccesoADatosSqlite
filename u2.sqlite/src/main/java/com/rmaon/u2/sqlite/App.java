c968403be03016bec7067f5aa54edbc7bf5055b3
Rmaon <ramoncamineroarroyo@gmail.com>
2023-10-18 16:18:14 +0200

    Commit inicial

---
+++ u2.sqlite/src/main/java/com/rmaon/u2/sqlite/App.java
@@ -0,0 +1,13 @@
+package com.rmaon.u2.sqlite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
